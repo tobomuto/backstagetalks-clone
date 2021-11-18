d2a5ff4943aed3b58d6fe9037216fa7740cd0495
Thomas Bouvier <tombvr75@gmail.com>
2021-11-18 13:44:22 +0100

    color changing logic

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <section class="issue#5"></section>
-    <p>{{ windowTop }}</p>
     <IssueSection v-for="issue in issues" :key="issue.title" :issue="issue"/>
 
 </div>
@@ -20,14 +18,35 @@ export default {
     return {
       issues: issuesJson,
       section: 0,
-      windowTop: 0
+      windowTop: 0,
+      windowSize: 0
     }
   },
   mounted() {
     let self = this;
+    const htmlTag = document.querySelector('html');
+    const sectionsTags = document.getElementsByTagName('section')
+    let sectionTop0 = sectionsTags[0].offsetTop;
+    let sectionTop1 = sectionsTags[1].offsetTop;
+    let sectionTop2 = sectionsTags[2].offsetTop;
+    let sectionTop3 = sectionsTags[3].offsetTop;
+    let sectionTop4 = sectionsTags[4].offsetTop;
+
     window.addEventListener("scroll", function() {
       self.windowTop = window.scrollY;
       self.windowSize = window.innerHeight;
+
+      if (self.windowTop == sectionTop0 && self.windowTop < sectionTop1) {
+        htmlTag.style.backgroundColor = "#00c1b5"
+      } else if (self.windowTop >= sectionTop1 && self.windowTop < sectionTop2) {
+        htmlTag.style.backgroundColor = "#ff651a";
+      } else if (self.windowTop >= sectionTop2 && self.windowTop < sectionTop3) {
+        htmlTag.style.backgroundColor = "#ffbe00";
+      } else if (self.windowTop >= sectionTop3 && self.windowTop < sectionTop4) {
+        htmlTag.style.backgroundColor = "#1d3fbb";
+      } else if (self.windowTop >= sectionTop4) {
+        htmlTag.style.backgroundColor = "#e30512";
+      }
     });
   },
 }

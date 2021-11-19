565b400a3cf9d7a44c5ee4b8d93c53e43a7c0919
Thomas Bouvier <tombvr75@gmail.com>
2021-11-19 01:20:28 +0100

    enhanced offset point for color changing

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -26,17 +26,17 @@ export default {
     let self = this;
     const htmlTag = document.querySelector('html');
     const sectionsTags = document.getElementsByTagName('section')
-    let sectionTop0 = sectionsTags[0].offsetTop;
-    let sectionTop1 = sectionsTags[1].offsetTop;
-    let sectionTop2 = sectionsTags[2].offsetTop;
-    let sectionTop3 = sectionsTags[3].offsetTop;
-    let sectionTop4 = sectionsTags[4].offsetTop;
+    let sectionTop0 = sectionsTags[0].offsetTop - 300;
+    let sectionTop1 = sectionsTags[1].offsetTop - 300;
+    let sectionTop2 = sectionsTags[2].offsetTop - 300;
+    let sectionTop3 = sectionsTags[3].offsetTop - 300;
+    let sectionTop4 = sectionsTags[4].offsetTop - 300;
 
     window.addEventListener("scroll", function() {
       self.windowTop = window.scrollY;
       self.windowSize = window.innerHeight;
 
-      if (self.windowTop == sectionTop0 && self.windowTop < sectionTop1) {
+      if (self.windowTop >= sectionTop0 && self.windowTop < sectionTop1) {
         htmlTag.style.backgroundColor = "#00c1b5"
       } else if (self.windowTop >= sectionTop1 && self.windowTop < sectionTop2) {
         htmlTag.style.backgroundColor = "#ff651a";
